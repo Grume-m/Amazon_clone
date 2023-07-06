6e61b2f70361110095ae93d245d2f0a05c8e3f4a
Grume-m <mekonnengrume@gmail.com>
2023-07-06 13:45:02 -0700

    axios base url changed

--- AmazonClone-Frontend/amazon-clone/src/axios.jsx
+++ AmazonClone-Frontend/amazon-clone/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:10000",
+  baseURL: "https://grume-amazon.cyclic.app",
   // https://us-centrall-clone-d9842.cloudfunctions.net/api
 });
 
